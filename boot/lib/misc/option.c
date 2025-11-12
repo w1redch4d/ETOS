882f2717c356e68d5c3d661af8ee1cc9ac251a79
Kaimakan71 <90975047+Kaimakan71@users.noreply.github.com>
2025-11-12 16:25:00 -0500

    boot/lib: BcdUtilGetBootOption and BlGetBootOptionBoolean.
    Signed-off-by: Kaimakan71 <90975047+Kaimakan71@users.noreply.github.com>

--- boot/lib/misc/option.c
+++ boot/lib/misc/option.c
@@ -16,6 +16,66 @@ Abstract:
 
 #include "bootlib.h"
 
+PBOOT_OPTION_CALLBACKS BlpBootOptionCallbacks = NULL;
+ULONGLONG BlpBootOptionCallbackCookie;
+
+PBOOT_ENTRY_OPTION
+BcdUtilGetBootOption (
+    IN PBOOT_ENTRY_OPTION Options,
+    IN BCDE_DATA_TYPE     Type
+    )
+
+/*++
+
+Routine Description:
+
+    Retrieves a boot option of type Type.
+
+Arguments:
+
+    Options - Pointer to the boot option list.
+
+    Type - The type of option to search for.
+
+Return Value:
+
+    Pointer to the option if found.
+
+    NULL if not found.
+
+--*/
+
+{
+    ULONG NextOptionOffset;
+    PBOOT_ENTRY_OPTION Option;
+
+    if (Options == NULL) {
+        return NULL;
+    }
+
+    NextOptionOffset = 0;
+    do {
+        Option = (PBOOT_ENTRY_OPTION)((PUCHAR)Options + NextOptionOffset);
+        if (Option->Type == Type && !Option->IsInvalid) {
+            return Option;
+        }
+
+        //
+        // Recursively search additional options.
+        //
+        if (Option->AdditionalOptionsOffset != 0) {
+            Option = BcdUtilGetBootOption((PBOOT_ENTRY_OPTION)((PUCHAR)Option + Option->AdditionalOptionsOffset), Type);
+            if (Option != NULL) {
+                return Option;
+            }
+        }
+
+        NextOptionOffset = Option->NextOptionOffset;
+    } while (NextOptionOffset != 0);
+
+    return NULL;
+}
+
 ULONG
 BlGetBootOptionSize (
     IN PBOOT_ENTRY_OPTION Option
@@ -46,6 +106,9 @@ Return Value:
         TotalSize = sizeof(BOOT_ENTRY_OPTION);
     }
 
+    //
+    // Recursively add size of additional options.
+    //
     if (Option->AdditionalOptionsOffset != 0) {
         TotalSize += BlGetBootOptionListSize((PBOOT_ENTRY_OPTION)((PUCHAR)Option + Option->AdditionalOptionsOffset));
     }
@@ -87,4 +150,76 @@ Return Value:
     } while (Offset != 0);
 
     return TotalSize;
+}
+
+NTSTATUS
+BlGetBootOptionBoolean (
+    IN  PBOOT_ENTRY_OPTION Options,
+    IN  BCDE_DATA_TYPE     Type,
+    OUT PBOOLEAN           Value
+    )
+
+/*++
+
+Routine Description:
+
+    Retrieves a boot option of type Type as a BOOLEAN.
+
+Arguments:
+
+    Options - Pointer to the boot option list.
+
+    Type - The type of option to search for.
+
+    Value - Pointer to a BOOLEAN that receives the option's value.
+
+Return Value:
+
+    STATUS_SUCCESS if successful.
+
+    STATUS_INVALID_PARAMETER if Type is not of format BCDE_FORAMT_BOOLEAN.
+
+    STATUS_NOT_FOUND if no matching option could be found.
+
+    Any other status value returned by the callback.
+
+--*/
+
+{
+    NTSTATUS Status;
+    PBOOT_ENTRY_OPTION Option;
+    BOOLEAN Data;
+
+    if ((Type & BCDE_FORMAT_MASK) != BCDE_FORMAT_BOOLEAN) {
+        return STATUS_INVALID_PARAMETER;
+    }
+
+    Option = BcdUtilGetBootOption(Options, Type);
+    if (Option != NULL) {
+        Status = STATUS_SUCCESS;
+        Data = *(PBOOLEAN)((PUCHAR)Option + Option->DataOffset);
+    } else {
+        Status = STATUS_NOT_FOUND;
+    }
+
+    //
+    // Pass through BCD filter callback if registered.
+    //
+    if (BlpBootOptionCallbacks != NULL && BlpBootOptionCallbacks->Boolean != NULL) {
+        Status = BlpBootOptionCallbacks->Boolean(
+            BlpBootOptionCallbackCookie,
+            Status,
+            0,
+            BlGetApplicationIdentifier(),
+            Type,
+            NULL,
+            &Data
+        );
+    }
+
+    if (NT_SUCCESS(Status)) {
+        *Value = Data;
+    }
+
+    return Status;
 }
