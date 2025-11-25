0b820423c1f05ebad2277f11a5586ebdec7ed8e8
Kaimakan71 <90975047+Kaimakan71@users.noreply.github.com>
2025-11-24 21:18:36 -0500

    sdk/crt: Revise stdarg.h and stddef.h.
    Signed-off-by: Kaimakan71 <90975047+Kaimakan71@users.noreply.github.com>

--- sdk/inc/crt/stdarg.h
+++ sdk/inc/crt/stdarg.h
@@ -10,28 +10,81 @@ Module Name:
 
 Abstract:
 
-    Provides variable argument list definitions.
+    Variable argument list definitions.
 
 --*/
 
-#pragma once
+#if !defined(__STDARG_H)      || __has_feature(modules)    || \
+    defined(__need___va_list) || defined(__need_va_list)   || \
+    defined(__need_va_arg)    || defined(__need___va_copy) || \
+    defined(__need_va_copy)
 
-#ifndef _STDARG_H
-#define _STDARG_H 1
+//
+// Process definition requests.
+//
+#if !defined(__need___va_list) && !defined(__need_va_list)   && \
+    !defined(__need_va_arg)    && !defined(__need___va_copy) && \
+    !defined(__need_va_copy)
 
-#if defined(__cplusplus)
-extern "C" {
+    #define __STDARG_H
+    #define __need___va_list
+    #define __need_va_list
+    #define __need_va_arg
+    #define __need___va_copy
+
+    #if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || \
+        (defined(__cplusplus)      && __cplusplus      >= 201103L) || \
+        !defined(__STRICT_ANSI)
+        #define __need_va_copy
+    #endif
+
 #endif
 
-typedef __builtin_va_list va_list;
+//
+// Define requested types and macros.
+//
 
-#define va_start(ap, v) __builtin_va_start(ap, v)
-#define va_arg(ap, t)   __builtin_va_arg(ap, t)
-#define va_copy(ap, s)  __builtin_va_copy(ap, s)
-#define va_end(ap)      __builtin_va_end(ap)
-
-#if defined(__cplusplus)
-}
+#ifdef __need___va_list
+    #ifndef __GNUC_VA_LIST
+        #define __GNUC_VA_LIST
+        typedef __builtin_va_list __gnuc_va_list;
+    #endif
+    #undef __need___va_list
 #endif
 
-#endif /* !_STDARG_H */
+#ifdef __need_va_list
+    #ifndef _VA_LIST
+        #define _VA_LIST
+        typedef __builtin_va_list va_list;
+    #endif
+    #undef __need_va_list
+#endif
+
+#ifdef __need_va_arg
+    #ifndef va_arg
+        #define va_arg(ap, type) __builtin_va_arg(ap, type)
+        #define va_end(ap)       __builtin_va_end(ap)
+        #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
+            #define va_start(ap, ...)   __builtin_va_start(ap, 0)
+        #else
+            #define va_start(ap, param) __builtin_va_start(ap, param)
+        #endif
+    #endif
+    #undef __need_va_arg
+#endif
+
+#ifdef __need___va_copy
+    #ifndef __va_copy
+        #define __va_copy(dest, src) __builtin_va_copy(dest, src)
+    #endif
+    #undef __need___va_copy
+#endif
+
+#ifdef __need_va_copy
+    #ifndef va_copy
+        #define va_copy(dest, src) __builtin_va_copy(dest, src)
+    #endif
+    #undef __need_va_copy
+#endif
+
+#endif
--- sdk/inc/crt/stddef.h
+++ sdk/inc/crt/stddef.h
@@ -10,60 +10,162 @@ Module Name:
 
 Abstract:
 
-    Provides standard types and definitions.
+    Standard type and macro definitions.
 
 --*/
 
-#pragma once
+#if !defined(__STDDEF_H)        || __has_feature(modules)      || \
+    defined(__need_ptrdiff_t)   || defined(__need_size_t)      || \
+    defined(__need_rsize_t)     || defined(__need_wchar_t)     || \
+    defined(__need_nullptr_t)   || defined(__need_max_align_t) || \
+    defined(__need_wint_t)      || defined(__need_NULL)        || \
+    defined(__need_unreachable) || defined(__need_offsetof)    || \
+    (defined(__STDC_WANT_LIB_EXT1__) && __STDC_WANT_LIB_EXT1__ > 0)
 
-#ifndef _STDDEF_H
-#define _STDDEF_H 1
+//
+// Process definition requests.
+//
+#if !defined(__need_ptrdiff_t)   && !defined(__need_size_t)      && \
+    !defined(__need_rsize_t)     && !defined(__need_wchar_t)     && \
+    !defined(__need_nullptr_t)   && !defined(__need_max_align_t) && \
+    !defined(__need_wint_t)      && !defined(__need_NULL)        && \
+    !defined(__need_unreachable) && !defined(__need_offsetof)
 
-#if defined(__cplusplus)
-extern "C" {
+    #define __STDDEF_H
+    #define __need_ptrdiff_t
+    #define __need_size_t
+    #define __need_wchar_t
+    #define __need_NULL
+    #define __need_offsetof
+
+    #if defined(__STDC_WANT_LIB_EXT1__) && __STDC_WANT_LIB_EXT1__ > 0
+        #define __need_rsize_t
+    #endif
+
+    #if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L) || defined(__cplusplus)
+        #define __need_nullptr_t
+    #endif
+
+    #if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L) || (defined(__cplusplus) && __cplusplus >= 201103L)
+        #define __need_max_align_t
+    #endif
+
+    #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
+        #define __need_unreachable
+    #endif
+
 #endif
 
-#ifndef NULL
-    #if defined(__cplusplus)
-        #if __cplusplus >= 201103L
-            #define NULL nullptr
-        #else
-            #define NULL 0
+//
+// Define requested types and macros.
+//
+
+#ifdef __need_ptrdiff_t
+    #if !defined(_PTRDIFF_T) || (__has_feature(modules) && !__building_module(_Builtin_stddef))
+        #define _PTRDIFF_T
+        typedef __PTRDIFF_TYPE__ ptrdiff_t;
+    #endif
+    #undef __need_ptrdiff_t
+#endif
+
+#ifdef __need_size_t
+    #if !defined(_SIZE_T) || (__has_feature(modules) && !__building_module(_Builtin_stddef))
+        #define _SIZE_T
+        typedef __SIZE_TYPE__ size_t;
+    #endif
+    #undef __need_size_t
+#endif
+
+#ifdef __need_rsize_t
+    #if !defined(_RSIZE_T) || (__has_feature(modules) && !__building_module(_Builtin_stddef))
+        #define _RSIZE_T
+        typedef __SIZE_TYPE__ rsize_t;
+    #endif
+    #undef __need_rsize_t
+#endif
+
+#ifdef __need_wchar_t
+    #if (!defined(__cplusplus) || (defined(_MSC_VER) && !_NATIVE_WCHAR_T_DEFINED)) && \
+        (!defined(_WCHAR_T) || (__has_feature(modules) && !__building_module(_Builtin_stddef)))
+        #define _WCHAR_T
+        typedef __WCHAR_TYPE__ wchar_t;
+        #ifdef _MSC_EXTENSIONS
+            #define _WCHAR_T_DEFINED
         #endif
-    #else
-        #define NULL ((void *) 0)
     #endif
+    #undef __need_wchar_t
 #endif
 
-#ifndef _SIZE_T_DEFINED
-    #define _SIZE_T_DEFINED
-    #if defined(_WIN64)
-        typedef unsigned __int64 size_t;
-    #else
-        typedef unsigned int size_t;
+#ifdef __need_nullptr_t
+    #if !defined(_NULLPTR_T) || (__has_feature(modules) && !__building_module(_Builtin_stddef))
+        #define _NULLPTR_T
+        #ifdef __cplusplus
+            #if defined(_MSC_EXTENSIONS) && defined(_NATIVE_NULLPTR_SUPPORTED)
+                namespace std {
+                    typedef decltype(nullptr) nullptr_t;
+                }
+                using ::std::nullptr_t;
+            #endif
+        #elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
+            typedef typeof(nullptr) nullptr_t;
+        #endif
     #endif
+    #undef __need_nullptr_t
 #endif
 
-#ifndef _PTRDIFF_T_DEFINED
-    #define _PTRDIFF_T_DEFINED
-    #if defined(_WIN64)
-        typedef __int64 ptrdiff_t;
-    #else
-        typedef long int ptrdiff_t;
+#ifdef __need_max_align_t
+    #ifndef __LIBC_MAX_ALIGN_T_DEFINED
+        #define __LIBC_MAX_ALIGN_T_DEFINED
+        #if defined(_MSC_VER)
+            typedef double max_align_t;
+        #elif defined(__APPLE__)
+            typedef long double max_align_t;
+        #else
+            typedef struct {
+                long long   __libc_max_align_ll __attribute__((__aligned__(__alignof__(long long))));
+                long double __libc_max_align_ld __attribute__((__aligned__(__alignof__(long double))));
+            } max_align_t;
+        #endif
     #endif
+    #undef __need_max_align_t
 #endif
 
-#ifndef _WCHAR_T_DEFINED
-    #define _WCHAR_T_DEFINED
-    typedef unsigned short wchar_t;
+#ifdef __need_wint_t
+    #ifndef _WINT_T
+        #define _WINT_T
+        typedef __WINT_TYPE__ wint_t;
+    #endif
+    #undef __need_wint_t
 #endif
 
-#ifndef offsetof
-    #define offsetof(type, member) __builtin_offsetof(type, member)
+#ifdef __need_NULL
+    #if !defined(NULL) || (__has_feature(modules) && !__building_module(_Builtin_stddef))
+        #undef NULL
+        #ifdef __cplusplus
+            #if !defined(__MINGW32__) && !defined(_MSC_VER)
+                #define NULL __null
+            #else
+                #define NULL 0
+            #endif
+        #else
+            #define NULL ((void *) 0)
+        #endif
+    #endif
+    #undef __need_NULL
 #endif
 
-#if defined(__cplusplus)
-}
+#ifdef __need_unreachable
+    #if !defined(unreachable) || (__has_feature(modules) && !__building_module(_Builtin_stddef))
+        #define unreachable() __builtin_unreachable()
+    #endif
+    #undef __need_unreachable
 #endif
 
-#endif /* !_STDDEF_H */
+#ifdef __need_offsetof
+    #if !defined(offsetof) || (__has_feature(modules) && !__building_module(_Builtin_stddef))
+        #define offsetof(type, member) __builtin_offsetof(type, member)
+    #endif
+    #undef __need_offsetof
+#endif
+
+#endif
